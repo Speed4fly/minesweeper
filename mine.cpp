9b9f1d10b0deb73b3240b7164064ea9f7903d493
WJP <wangjiping@iie.ac.cn>
2021-06-09 13:05:11 +0800

    first release

---
+++ mine.cpp
@@ -0,0 +1,55 @@
+﻿// mine.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "mineweeper.h"
+#include<iostream>
+using namespace std;
+
+int main() {
+	int x, y, difficulty, length, mode;
+	cout << "输入游戏难度(小于100的十进制整数)，来作为生成雷的概率。" << endl;
+	cin >> difficulty;
+	cout << "输入游戏棋盘宽度，推荐用小于10的整数。" << endl;
+	cin >> length;
+	map4minesweeper m;
+	m.map(length, difficulty);
+	while (true)
+	{
+		m.check40();
+		m.display();
+		cout << "输入探测模式：\n0:扫描(若扫描点有雷则直接爆炸)\n1:标记为地雷" << endl;
+		cin >> mode;
+		cout << "输入想要探测的坐标(空格分割)。" << endl;
+		cin >> x >> y;
+		if (mode == 1) {
+			m.flag(y, x);
+		}
+		else {
+			if (m.click(y, x) == 0) {
+				cout << "BOOM!" << endl;
+				m.show_bomb();
+				system("pause");
+				return 0;
+			}
+		}
+		if (m.is_success() == 1) {
+			cout << "SUCCESS!" << endl;
+			m.show_bomb();
+			system("pause");
+			return 0;
+		}
+
+
+	}
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ mineweeper.h
@@ -0,0 +1,196 @@
+
+#include "stdlib.h"
+#include<vector>
+#include<iostream>
+#include<string>
+#include<time.h>
+
+
+using namespace std;
+class map4minesweeper {
+private:
+	int length{};
+	//int difficulty;
+	vector<vector<int>> map_data;
+	vector<vector<int>> map_indicater;
+	vector<vector<string>> map_display;
+public:
+	map4minesweeper() {
+
+	}
+	void map(int length,int difficulty) {
+		srand((int)time(0));
+		this->length = length;
+		vector<int> tmp;
+		vector<string> c_tmp;
+			for (int index = 0; index < length; index++) {
+				this->map_data.push_back(tmp);
+				this->map_display.push_back(c_tmp);
+				for (int sub_index = 0; sub_index < length; sub_index++) {
+					this->map_display[index].push_back(".");
+					if (rand() % 100 > difficulty) this->map_data[index].push_back(0);
+					else this->map_data[index].push_back(1);
+				}
+			}
+			for (int index = 0; index < length; index++) {
+				this->map_indicater.push_back(tmp);
+				for (int sub_index = 0; sub_index < length; sub_index++) {
+					int sum = 0;
+					for (int x = index - 1; x < index + 2; x++) {
+						for (int y = sub_index - 1; y < sub_index + 2; y++) {
+							if (x >= 0 && x < length && y >= 0 && y < length) sum += this->map_data[x][y];
+
+						}
+					}
+					sum -= this->map_data[index][sub_index];
+					this->map_indicater[index].push_back(sum);
+				}
+		}
+	}
+	void cleary(int x, int y) {
+		int sub_index = y;
+		while (sub_index>=0)
+		{
+			if (this->map_indicater[x][sub_index] == 0) {
+				this->map_display[x][sub_index] = to_string(this->map_indicater[x][sub_index]);
+				sub_index -= 1;
+			}
+			else {
+				this->map_display[x][sub_index] = to_string(this->map_indicater[x][sub_index]);
+				break;
+			}
+		}
+		sub_index = y + 1;
+		while (sub_index < this->length)
+		{
+			if (this->map_indicater[x][sub_index] == 0) {
+				this->map_display[x][sub_index] = to_string(this->map_indicater[x][sub_index]);
+				sub_index += 1;
+			}
+			else {
+				this->map_display[x][sub_index] = to_string(this->map_indicater[x][sub_index]);
+				break;
+			}
+		}
+
+	}
+	void clear(int x, int y) {
+		int index = x;
+		while (index>=0)
+		{
+			if (this->map_indicater[index][y] == 0) {
+				this->map_display[index][y] = to_string(this->map_indicater[index][y]);
+				this->cleary(index, y);
+				index -= 1;
+			}
+			else {
+				this->map_display[index][y] = to_string(this->map_indicater[index][y]);
+				break;
+			}
+		}
+		index = x + 1;
+		while (index >this->length)
+		{
+			if (this->map_indicater[index][y] == 0) {
+				this->map_display[index][y] = to_string(this->map_indicater[index][y]);
+				this->cleary(index, y);
+				index += 1;
+			}
+			else {
+				this->map_display[index][y] = to_string(this->map_indicater[index][y]);
+				break;
+			}
+		}
+	}
+	int click(int x, int y) {
+		if (x >= this->length || y >= this->length) {
+			cout << "无效坐标" << endl;
+			return 1;
+		}
+		else {
+			if (this->map_data[x][y] == 0) {
+				this->clear(x, y);
+				return 1;
+			}
+			else
+				return 0;
+		}
+	}
+	void display() {
+		cout << "LINE   ";
+		for (int index = 0; index < this->length; index++) {
+			cout << to_string(index) << ' ';
+		}
+		cout << endl;
+		for (int index = 0; index < this->length; index++) {
+			cout << "Line" << to_string(index) << "  ";
+			for (int sub_index = 0; sub_index < this->length; sub_index++) {
+				cout << this->map_display[index][sub_index] << ' ';
+			}
+			cout << endl;
+		}
+	}
+	void check40() {
+		int flag = 0;
+		for (int index = 0; index < this->length; index++) {
+			for (int sub_index = 0; sub_index < this->length; sub_index++) {
+				if (this->map_display[index][sub_index] == ".") {
+					for (int x = index - 1; x < index + 2; x++) {
+						for (int y = sub_index - 1; y < sub_index + 2; y++) {
+							if (x >= 0 && x < this->length && y >= 0 && y < this->length) {
+								if (this->map_display[x][y] == "0") {
+									this->map_display[index][sub_index] = to_string(this->map_indicater[index][sub_index]);
+									flag = 1;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		if (flag == 1)
+			this->check40();
+	}
+	void show_bomb(){
+		int wrong_flag = 0;
+		for (int index = 0; index < this->length; index++) {
+			for (int sub_index = 0; sub_index < this->length; sub_index++) {
+				if (this->map_display[index][sub_index] == "!"&&this->map_data[index][sub_index]==0) {
+					wrong_flag += 1;
+				}
+				if (this->map_data[index][sub_index] == 1) {
+					if (this->map_display[index][sub_index] == "!") {
+						this->map_display[index][sub_index] = "v";
+					}
+					else
+						this->map_display[index][sub_index] = "X";
+				}
+				else
+				{
+					this->map_display[index][sub_index] = to_string(this->map_indicater[index][sub_index]);
+				}
+			}
+		}
+		this->display();
+		cout << "错误标记" << to_string(wrong_flag) << "个。" << endl;
+	}
+	int is_success() {
+		int flag = 0;
+		for (int index = 0; index < this->length; index++) {
+			for (int sub_index = 0; sub_index < this->length; sub_index++) {
+				if (this->map_data[index][sub_index] == 0&& this->map_display[index][sub_index] == ".") {
+					flag = 1;
+					break;
+				}
+			}
+		}
+		if (flag == 0) {
+			return 1;
+		}
+		return 0;
+	}
+	void flag(int x, int y) {
+		this->map_display[x][y] = "!";
+	}
+	
+};
